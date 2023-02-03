dcf36ac3b0a6a69ad667182a5e07278b9e7b727f
LLOlaBB <lauracbarros@hotmail.com>
2023-02-03 15:58:01 -0300

    Cria atualizações de status

--- ListaTarefas/assets/js/index.js
+++ ListaTarefas/assets/js/index.js
@@ -34,6 +34,14 @@ function updateViewTable(List) {
     tbodyTask.innerHTML = "";
     List.forEach((item, index) => {
         const trElement = document.createElement("tr");
+
+        if (item.status) {
+            const myClassStatus = getClassByStatus(item.status);
+            if (myClassStatus !== "") {
+                trElement.classList.add(myClassStatus);
+            }
+        }
+
         trElement.innerHTML = `
             <td>${index + 1}</td>
             <td>${item.title}</td>
@@ -43,7 +51,11 @@ function updateViewTable(List) {
                     <img class="dropdown-toggle icon-button" data-bs-toggle="dropdown" src="./assets/icons/more.svg">
                     <ul class="dropdown-menu ">
                         <li><h6 class="dropdown-header">Ações</h6></li>
-                        <li><button class="dropdown-item">Excluir</button></li>
+                        <li><button class="dropdown-item" onclick="deleteTask(${index})">Excluir</button></li>
+                        <li><h6 class="dropdown-header">Status</h6></li>
+                        <li><button class="btn btn-outline-success btn-status-task" onclick="updateStatusTask(${index}, 'concluida')">Concluída</button></li>
+                        <li><button class="btn btn-outline-warning btn-status-task" onclick="updateStatusTask(${index}, 'pendente')">Pendente</button></li>
+                        <li><button class="btn btn-outline-danger btn-status-task" onclick="updateStatusTask(${index}, 'cancelada')">Cancelada</button></li>
                     </ul>
                 </div>
             </td>
@@ -66,9 +78,32 @@ function getTasksLocaStorage() {
     } catch (exception) {
         if (exception !== "sem dados") {
             alert("Não foi possível recuperar sua lista de tarefas");
-
         }
         return [];
     }
-
+}
+function deleteTask(index) {
+    tasks.splice(index, 1);
+    updateViewTable(tasks);
+    saveTasksLocaStorage();
+}
+function updateStatusTask(index, status) {
+    tasks[index].status = status;
+    saveTasksLocaStorage();
+    updateViewTable(tasks);
+}
+function getClassByStatus(status) {
+    switch (status) {
+        case 'concluida':
+            return 'table-success';
+            break;
+        case 'pendente':
+            return 'table-warning';
+            break;
+        case 'cancelada':
+            return 'table-danger';
+            break;
+        default:
+            return "";
+    }
 }
